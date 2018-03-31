b7629434bb23fc1e22a92756065945668826d3fc
ganchaoyang <gancy.programmer@gmail.com>
2018-03-31 11:05:53 +0800

    fix:修改路径里的IP为localhost

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,6 @@
 module.exports = {
 	NODE_ENV: '"development"',
 	ENV_CONFIG: '"dev"',
-	BASE_API: '"http://192.168.9.180:8088/"'
+	BASE_API: '"http://localhost:8088/"'
 	// BASE_API: '"https://api-dev"'
 }
