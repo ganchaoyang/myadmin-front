2f1c10f3c54500d4b7d9cd86f4b725356d6e6ffe
ganchaoyang <gancy.programmer@gmail.com>
2018-04-18 17:35:41 +0800

    add:实现查询。

--- src/views/perm/index.vue
+++ src/views/perm/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="app-container calendar-list-container">
+    <div class="filter-container">
+        <el-input style="width: 300px;" class="filter-item" :placeholder="$t('table.permName')" v-model="queryPermName"></el-input>
+        <el-button class="filter-item" type="primary" icon="el-icon-search" @click="searchHandle">{{$t('table.search')}}</el-button>
+        <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit">{{$t('table.add')}}</el-button>
+      </div>
     <tree-table :data="list" :evalFunc="func" :expandAll="expandAll" border>
       <el-table-column :label="$t('table.permName')">
           <template slot-scope="scope">
@@ -41,7 +46,8 @@ export default {
       func: treeToArray,
       expandAll: false,
       data: [],
-      list: []
+      list: [],
+      queryPermName: ''
     }
   },
   created() {
@@ -53,6 +59,15 @@ export default {
         this.data = Response.data.data
         this.list = Response.data.data
       })
+    },
+    searchHandle() {
+      var temp = []
+      this.data.forEach(element => {
+        if (element.name.match(this.queryPermName)) {
+          temp.push(element)
+        }
+      })
+      this.list = temp
     }
   }
 }
